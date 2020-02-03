3bad394b2e2d8d3f5b9907b74a4379f6b54ee9e1
Steven Olsen <solsen@aspireprecollege.com>
2020-02-03 08:59:57 -0600

    20200203 tweaked stripe js call

--- public/site/themes/precollege/js/precollege.js
+++ public/site/themes/precollege/js/precollege.js
@@ -1,14 +1,12 @@
-var stripe = Stripe('pk_live_wO76DSR6IGWdN7Tz7qgJvFSz00mXuJGhGr');
+function callStripe(campSKU) {
+	var stripe = Stripe('pk_live_wO76DSR6IGWdN7Tz7qgJvFSz00mXuJGhGr');
 
-window.addEventListener('message', event => {
-   if(event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormSubmit') {
-       stripe.redirectToCheckout({
-         items: [{
-           sku: 'sku_GfI8c62sCyScYY',
-           quantity: 1
-         }],
-         successUrl: 'https://precollege.lsu.edu/thankyou',
-         cancelUrl: 'https://precollege.lsu.edu/cancel'
-       });
-   }
-});
+   	stripe.redirectToCheckout({
+	     items: [{
+	       sku: campSKU,
+	       quantity: 1
+	     }],
+	     successUrl: 'https://precollege.lsu.edu/thankyou',
+	     cancelUrl: 'https://precollege.lsu.edu/cancel'
+   	});
+}
